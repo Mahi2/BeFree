fa93601e3a651c33225ceb4cd88802159c336154
Mahi2 <mahi2hm@outlook.fr>
2018-08-23 17:46:05 +0200

    [Add] Adding settings for languages

---
+++ install/settings.inc.php
@@ -0,0 +1,10 @@
+<?php
+define("DEFAULT_LANGUAGE", "en");
+
+// Array of available languages
+$arr_active_languages = array(
+    "en" => "English",
+    "bg" => "Български",
+    "es" => "Spanish",
+    "de" => "German"
+);
