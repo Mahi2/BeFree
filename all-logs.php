86f22737da19e5278d652d1b4bc1f493589b65a6
Mahi2 <mahi2hm@outlook.fr>
2018-08-23 18:22:46 +0200

    [Update] View part of all-logs

--- all-logs.php
+++ all-logs.php
@@ -13,3 +13,145 @@ if (isset($_GET['delete-all'])) {
     $query = $mysqli->query("TRUNCATE TABLE `$table`");
 }
 ?>
+<div class="content-wrapper">
+
+			<!--CONTENT CONTAINER-->
+			<!--===================================================-->
+			<div class="content-header">
+
+				<div class="container-fluid">
+				  <div class="row mb-2">
+        		    <div class="col-sm-6">
+        		      <h1 class="m-0 text-dark"><i class="fas fa-align-justify"></i> All Logs</h1>
+        		    </div>
+        		    <div class="col-sm-6">
+        		      <ol class="breadcrumb float-sm-right">
+        		        <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i> Admin Panel</a></li>
+        		        <li class="breadcrumb-item active">All Logs</li>
+        		      </ol>
+        		    </div>
+        		  </div>
+    			</div>
+            </div>
+
+				<!--Page content-->
+				<!--===================================================-->
+				<div class="content">
+				<div class="container-fluid">
+
+                <div class="row">
+				<div class="col-md-12">
+
+				    <div class="card">
+						<div class="card-header">
+							<h3 class="card-title">All Logs</h3>
+						</div>
+						<div class="card-body">
+
+                                    <center><a href="?delete-all" class="btn btn-flat btn-danger" title="Delete all logs"><i class="fas fa-trash"></i> Delete All</a></center>
+
+<table id="dt-basic" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+									<thead>
+										<tr>
+								          <th><i class="fas fa-list-ol"></i> ID</th>
+						                  <th><i class="fas fa-user"></i> IP Address</th>
+						                  <th><i class="fas fa-calendar"></i> Date</th>
+										  <th><i class="fas fa-globe"></i> Browser</th>
+										  <th><i class="fas fa-desktop"></i> OS</th>
+										  <th><i class="fas fa-map"></i> Country</th>
+						                  <th><i class="fas fa-bomb"></i> Type</th>
+										  <th><i class="fas fa-cog"></i> Actions</th>
+										</tr>
+									</thead>
+									<tbody>
+<?php
+$table = $prefix . 'logs';
+$sql   = $mysqli->query("SELECT id, ip, date, browser, browser_code, os, os_code, country, country_code, type FROM `$table` ORDER by id DESC");
+while ($row = mysqli_fetch_assoc($sql)) {
+    echo '
+										<tr>
+                                          <td>' . $row['id'] . '</td>
+						                  <td>' . $row['ip'] . '</td>
+						                  <td>' . $row['date'] . '</td>
+										  <td><img src="assets/img/icons/browser/' . $row['browser_code'] . '.png" /> ' . $row['browser'] . '</td>
+										  <td><img src="assets/img/icons/os/' . $row['os_code'] . '.png" /> ' . $row['os'] . '</td>
+										  <td><img src="assets/plugins/flags/blank.png" class="flag flag-' . strtolower($row['country_code']) . '" alt="' . $row['country'] . '" /> ' . $row['country'] . '</td>
+						                  <td>';
+    if ($row['type'] == 'SQLi') {
+        echo '
+						                    <i class="fas fa-code"></i> ' . $row['type'] . '
+						                    ';
+    } else if ($row['type'] == 'Proxy') {
+        echo '
+						                    <i class="fas fa-globe"></i> ' . $row['type'] . '
+						                    ';
+    } else if ($row['type'] == 'Mass Requests') {
+        echo '
+						                    <i class="fas fa-retweet"></i> ' . $row['type'] . '
+						                    ';
+    } else if ($row['type'] == 'Spammer') {
+        echo '
+						                    <i class="fas fa-keyboard"></i> ' . $row['type'] . '
+						                    ';
+    } else {
+        echo '
+						                    <i class="fas fa-user-secret"></i> ' . $row['type'] . '
+						                    ';
+    }
+    echo '
+										  </td>
+										  <td>
+                                            <a href="log-details.php?id=' . $row['id'] . '" class="btn btn-flat btn-primary"><i class="fas fa-tasks"></i> Details</a>
+											';
+    if (get_banned($row['ip']) == 1) {
+        echo '
+										    <a href="bans-ip.php?delete-id=' . get_bannedid($row['ip']) . '" class="btn btn-flat btn-success"><i class="fas fa-ban"></i> Unban</a>
+									        ';
+    } else {
+        echo '
+										    <a href="bans-ip.php?ip=' . $row['ip'] . '&reason=' . $row['type'] . '" class="btn btn-flat btn-warning"><i class="fas fa-ban"></i> Ban</a>
+									        ';
+    }
+    echo '
+											<a href="?delete-id=' . $row['id'] . '" class="btn btn-flat btn-danger"><i class="fas fa-times"></i> Delete</a>
+										  </td>
+										</tr>
+';
+}
+?>
+									</tbody>
+								    </table>
+
+                        </div>
+                     </div>
+                </div>
+
+				</div>
+
+				</div>
+				</div>
+				<!--===================================================-->
+				<!--End page content-->
+
+			</div>
+			<!--===================================================-->
+			<!--END CONTENT CONTAINER-->
+</div>
+<script>
+$(document).ready(function() {
+
+	$('#dt-basic').dataTable( {
+		"responsive": true,
+        "order": [[ 0, "desc" ]],
+		"language": {
+			"paginate": {
+			  "previous": '<i class="fas fa-angle-left"></i>',
+			  "next": '<i class="fas fa-angle-right"></i>'
+			}
+		}
+	} );
+} );
+</script>
+<?php
+footer();
+?>
