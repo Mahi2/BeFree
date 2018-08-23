b8755e19078255af57a9ea0ea8982e7ef36cfc19
Mahi2 <mahi2hm@outlook.fr>
2018-08-23 17:03:48 +0200

    [Add] Adding languages with languages files

---
+++ install/language/en.php
@@ -0,0 +1,29 @@
+<?php
+//------------------------------------------------------------------------------
+// English (EN)
+//------------------------------------------------------------------------------
+$arrLang = array();
+
+$arrLang['alert_remove_files']        = "For security reasons, please remove the <b>install/</b> folder from your server!";
+$arrLang['error_check_db_connection'] = "Database connecting error! Please check your connection parameters.<br />";
+$arrLang['database_info']             = "Below you should enter your database connection details. If you’re not sure about these, contact your host.";
+$arrLang['settings_info']             = "Please provide the following information. Don’t worry, you can always change these settings later.";
+
+$arrLang['installation_wizard'] = "Installation Wizard";
+$arrLang['choose_language']     = "Choose a Language";
+$arrLang['continue']            = "Continue";
+$arrLang['back_to_website']     = "Back to the Website";
+$arrLang['requirements']        = "Requirements";
+$arrLang['back']                = "Back";
+$arrLang['next']                = "Next";
+$arrLang['database_host']       = "Database Host";
+$arrLang['database_name']       = "Database Name";
+$arrLang['database_username']   = "Database Username";
+$arrLang['database_password']   = "Database Password";
+$arrLang['table_prefix']        = "Table Prefix";
+$arrLang['username']            = "Username";
+$arrLang['password']            = "Password";
+$arrLang['proceed']             = "Continue to BeFree";
+$arrLang['put_code']            = "Now put this code in a main file of your website to enable BeFree (Change \"projectsecurity_folder\" with the path on which you installed the product)";
+$arrLang['success_install']     = "BeFree has been successfully installed on your website!";
+?>
