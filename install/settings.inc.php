8abab2b6cdcddeb41ece198bea77e96c350d64c3
Mahi2 <mahi2hm@outlook.fr>
2018-08-23 17:48:53 +0200

    [Update] Config file directory, file name, template

--- install/settings.inc.php
+++ install/settings.inc.php
@@ -8,3 +8,16 @@ $arr_active_languages = array(
     "es" => "Spanish",
     "de" => "German"
 );
+
+// Config file directory - Directory, where config file must be
+define("CONFIG_FILE_DIRECTORY", "../");
+
+// Config file name - Output file with config parameters (database, username etc.)
+define("CONFIG_FILE_NAME", "config.php");
+
+// According to directory hierarchy (you may add/remove "../" before CONFIG_FILE_DIRECTORY)
+define("CONFIG_FILE_PATH", CONFIG_FILE_DIRECTORY . CONFIG_FILE_NAME);
+
+// Config file name - config template file name
+define("CONFIG_FILE_TEMPLATE", "config.tpl");
+?>
