27142dab1c170ac0ca0b79ff83e4e1d16bb58944
Mahi2 <mahi2hm@outlook.fr>
2018-08-23 17:23:32 +0200

    [Update] Updating core file with HTML content

--- install/core.php
+++ install/core.php
@@ -27,3 +27,42 @@ if (file_exists(CONFIG_FILE_PATH)) {
 function head()
 {
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>BeFree - <?php
+    echo lang_key("installation_wizard");
+?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/img/favicon.png">
+    <meta charset="utf-8">
+    <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" media="screen">
+    <link type="text/css" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="page-header">
+            <div class="row">
+                <div class="col-lg-12">
+                    <br /><center><h2><i class="fab fa-get-pocket"></i> BeFree - <?php
+    echo lang_key("installation_wizard");
+?></h2></center><br />
+                        <div class="jumbotron">
+<?php
+}
+
+function footer()
+{
+?>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
