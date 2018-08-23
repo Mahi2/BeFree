f2f2fc8f37ebee17a09e0e915d92f9d074270bd5
Mahi2 <mahi2hm@outlook.fr>
2018-08-23 17:08:25 +0200

    [Update] Finishing with languages files

---
+++ install/language/bg.php
@@ -0,0 +1,29 @@
+<?php
+//------------------------------------------------------------------------------
+// Bulgarian (BG)
+//------------------------------------------------------------------------------
+$arrLang = array();
+
+$arrLang['alert_remove_files']        = "От съображения за сигурност, моля, премахнете <b>install/</b> папката от вашия сървър!";
+$arrLang['error_check_db_connection'] = "Грешка при свързване с Базата Данни! Моля, проверете параметрите за връзка.<br />";
+$arrLang['database_info']             = "По-долу можете да въведете вашите данни за връзка с базата данни. Ако не сте сигурни за тях, свържете с вашия хостинг.";
+$arrLang['settings_info']             = "Моля, дайте следната информация. Не се притеснявайте, винаги можете да промените тези настройки по-късно.";
+
+$arrLang['installation_wizard'] = "Съветник за Инсталиране";
+$arrLang['choose_language']     = "Изберете Език";
+$arrLang['continue']            = "Продължение";
+$arrLang['back_to_website']     = "Назад към Сайта";
+$arrLang['requirements']        = "Изисквания";
+$arrLang['back']                = "Назад";
+$arrLang['next']                = "Напред";
+$arrLang['database_host']       = "Хост на Базата Данни";
+$arrLang['database_name']       = "Име на Базата Данни";
+$arrLang['database_username']   = "Потребителско Име на Базата Данни";
+$arrLang['database_password']   = "Парола на Базата Данни";
+$arrLang['table_prefix']        = "Представка на таблиците";
+$arrLang['username']            = "Администраторско Име";
+$arrLang['password']            = "Парола";
+$arrLang['proceed']             = "Продължете към Project SECURITY";
+$arrLang['put_code']            = "Сега поставете този код в някой главен файл на вашия сайт, за да активирате Project SECURITY (Променете \"projectsecurity_folder\" с пътя, където сте инсталирали продукта)";
+$arrLang['success_install']     = "Project SECURITY беше успешно инсталиран на вашия уебсайт!";
+?>
