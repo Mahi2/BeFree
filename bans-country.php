79f11d0c391578f50789e44bb30b867cbc0d2fa2
Mahi2 <mahi2hm@outlook.fr>
2018-08-28 11:28:05 +0200

    [Update] Html content

--- bans-country.php
+++ bans-country.php
@@ -18,5 +18,29 @@ if (isset($_GET['whitelist'])) {
     $query = $mysqli->query("UPDATE `$table` SET countryban_blacklist='0' WHERE id=1");
 }
 ?>
+<div class="content-wrapper">
 
+			<!--CONTENT CONTAINER-->
+			<!--===================================================-->
+			<div class="content-header">
+
+				<div class="container-fluid">
+				  <div class="row mb-2">
+        		    <div class="col-sm-6">
+        		      <h1 class="m-0 text-dark"><i class="fas fa-globe"></i> Country Bans</h1>
+        		    </div>
+        		    <div class="col-sm-6">
+        		      <ol class="breadcrumb float-sm-right">
+        		        <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i> Admin Panel</a></li>
+        		        <li class="breadcrumb-item active">Country Bans</li>
+        		      </ol>
+        		    </div>
+        		  </div>
+    			</div>
+            </div>
+
+				<!--Page content-->
+				<!--===================================================-->
+				<div class="content">
+				<div class="container-fluid">
 ?>
