74fa2e2573bb933c0b8be87b62f139ca591a0e90
Mahi2 <mahi2hm@outlook.fr>
2018-08-28 11:08:36 +0200

    [Update] If statement

--- badbots.php
+++ badbots.php
@@ -2,7 +2,27 @@
   require("core.php");
   head();
 
-  if(isset($_POST['save2'])){
-    $table = $prefix . 'badbot-settings';
+  if (isset($_POST['save2'])) {
+      $table = $prefix . 'badbot-settings';
+
+      if (isset($_POST['protection'])) {
+          $protection = 1;
+      } else {
+          $protection = 0;
+      }
+
+      if (isset($_POST['protection2'])) {
+          $protection2 = 1;
+      } else {
+          $protection2 = 0;
+      }
+
+      if (isset($_POST['protection3'])) {
+          $protection3 = 1;
+      } else {
+          $protection3 = 0;
+      }
+
+      $query = $mysqli->query("UPDATE `$table` SET protection='$protection', protection2='$protection2', protection3='$protection3' WHERE id=1");
   }
 ?>
