bfc47433dfb4a9293f49e4c2b91880ecd93e36ea
Mahi2 <mahi2hm@outlook.fr>
2018-08-29 10:21:27 +0200

    [Update] adding php code

--- bans-ip.php
+++ bans-ip.php
@@ -1,3 +1,15 @@
 <?php
-  require ("core.php");
+require("core.php");
+head();
+
+if (isset($_GET['delete-all'])) {
+    $table = $prefix . 'bans';
+    $query = $mysqli->query("TRUNCATE TABLE `$table`");
+}
+
+if (isset($_GET['delete-id'])) {
+    $id    = (int) $_GET["delete-id"];
+    $table = $prefix . 'bans';
+    $query = $mysqli->query("DELETE FROM `$table` WHERE id='$id'");
+}
 ?>
