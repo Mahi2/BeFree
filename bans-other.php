9f8258f46364fb341f319b7930bedd4d7d86d04a
Mahi2 <mahi2hm@outlook.fr>
2018-09-04 10:49:37 +0200

    [Update] Adding HTML content

---
+++ bans-other.php
@@ -0,0 +1,35 @@
+<?php
+  require("core.php");
+  head();
+
+  if (isset($_GET['delete-id'])) {
+    $id    = (int) $_GET["delete-id"];
+    $table = $prefix . 'bans-other';
+    $query = $mysqli->query("DELETE FROM `$table` WHERE id='$id'");
+  }
+?>
+<div class="content-wrapper">
+
+			<!--CONTENT CONTAINER-->
+			<!--===================================================-->
+			<div class="content-header">
+
+				<div class="container-fluid">
+				  <div class="row mb-2">
+        		    <div class="col-sm-6">
+        		      <h1 class="m-0 text-dark"><i class="fas fa-desktop"></i> Other Bans</h1>
+        		    </div>
+        		    <div class="col-sm-6">
+        		      <ol class="breadcrumb float-sm-right">
+        		        <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i> Admin Panel</a></li>
+        		        <li class="breadcrumb-item active">Other Bans</li>
+        		      </ol>
+        		    </div>
+        		  </div>
+    			</div>
+            </div>
+
+				<!--Page content-->
+				<!--===================================================-->
+				<div class="content">
+				<div class="container-fluid">
