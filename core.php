876fabfa6afe4f1744ca8c7879ae9e12460fc6b0
Mahi2 <mahi2hm@outlook.fr>
2018-09-05 16:54:06 +0200

    [Update] error reporting

--- core.php
+++ core.php
@@ -26,5 +26,12 @@
     exit;
 }
 
+if (basename($_SERVER['SCRIPT_NAME']) != 'html-encrypter.php' && basename($_SERVER['SCRIPT_NAME']) != 'warning-pages.php') {
+    $_GET  = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+}
 
+$table = $prefix . 'settings';
+$query = $mysqli->query("SELECT * FROM '$table' LIMIT 1");
+$row = mysqli_fetch_array($query);
 ?>
