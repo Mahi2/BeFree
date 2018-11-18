e82c0ae4b6f2b232955bba781f79161a7d5a5908
bernard-ng <ngandubernard@gmail.com>
2018-11-18 16:36:38 +0200

    [Update] setting up composer and class structure

---
+++ v2/src/Befree.php
@@ -0,0 +1,16 @@
+<?php
+namespace Befree;
+
+/**
+ * Class Befree
+ * @package Befree
+ */
+class Befree {
+    
+    /**
+     * the user configuration file
+     *
+     * @var string
+     */
+    public $configFile = 'config.php';
+}
