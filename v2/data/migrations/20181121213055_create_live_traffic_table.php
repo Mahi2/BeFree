79e3ce3bd51de088e6b591c76d9dcad7f89d96c1
bernard-ng <ngandubernard@gmail.com>
2018-11-26 01:23:53 +0200

    [Update] add migration logic in live-traffic

--- v2/data/migrations/20181121213055_create_live_traffic_table.php
+++ v2/data/migrations/20181121213055_create_live_traffic_table.php
@@ -2,6 +2,7 @@
 
 
 use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 class CreateLiveTrafficTable extends AbstractMigration
 {
@@ -32,6 +33,22 @@ class CreateLiveTrafficTable extends AbstractMigration
      */
     public function change()
     {
-
+        $this->table('live-traffic')
+            ->addColumn('ip', 'string', ['limit' => 15])
+            ->addColumn('useragent', 'string', ['limit' => MysqlAdapter::TEXT_SMALL])
+            ->addColumn('browser', 'string', ['limit' => MysqlAdapter::TEXT_SMALL])
+            ->addColumn('browser_code', 'string', ['limit' => 50])
+            ->addColumn('os', 'string', ['limit' => MysqlAdapter::TEXT_SMALL])
+            ->addColumn('os_code','string', ['limit' => 40])
+            ->addColumn('device_type', 'string', ['limit' => 12])
+            ->addColumn('country', 'string', ['limit' => 120])
+            ->addColumn('country_code', 'string', ['limit' => 2, 'default' => 'XX'])
+            ->addColumn('request_uri', 'string', ['limit' => MysqlAdapter::TEXT_SMALL])
+            ->addColumn('referer', 'string', ['limit' => MysqlAdapter::TEXT_SMALL])
+            ->addColumn('bot', 'integer', ['limit' => 1, 'default' => 0])
+            ->addColumn('date', 'string', ['limit' => 30])
+            ->addColumn('time', 'string', ['limit' => 5])
+            ->addColumn('uniquev', 'integer', ['limit' => 1, 'default' => 0])
+            ->create();
     }
 }
