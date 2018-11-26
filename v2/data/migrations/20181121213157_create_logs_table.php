6a2368f04815e5feb80f7025790a2b53445866bb
bernard-ng <ngandubernard@gmail.com>
2018-11-26 02:12:21 +0200

    [Update] add migration logic to logs

--- v2/data/migrations/20181121213157_create_logs_table.php
+++ v2/data/migrations/20181121213157_create_logs_table.php
@@ -2,6 +2,7 @@
 
 
 use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 class CreateLogsTable extends AbstractMigration
 {
@@ -32,6 +33,27 @@ class CreateLogsTable extends AbstractMigration
      */
     public function change()
     {
+        $this->table('logs')
+            ->addColumn('ip', 'string', ['limit' => 15])
+            ->addColumn('date', 'string', ['limit' => 30])
+            ->addColumn('time', 'string', ['limit' => 5])
+            ->addColumn('page', 'string', ['limit' => MysqlAdapter::TEXT_SMALL])
+            ->addColumn('query', 'string', ['limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('type', 'string', ['limit' => 50])
+            ->addColumn('browser', 'string', ['limit' => MysqlAdapter::TEXT_SMALL, 'default' => 'Unknown'])
+            ->addColumn('browser_code', 'string', ['limit' => 50])
+            ->addColumn('os', 'string', ['limit' => MysqlAdapter::TEXT_SMALL, 'default' => 'Unknown'])
+            ->addColumn('os_code', 'string', ['limit' => 40])
+            ->addColumn('country', 'string', ['limit' => 120, 'default' => 'Unknown'])
+            ->addColumn('contry_code', 'string', ['limit' => 2, 'default' => 'XX'])
+            ->addColumn('region', 'string', ['limit' => 120, 'default' => 'Unknown'])
+            ->addColumn('city', 'string', ['limit' => 120, 'default' => 'Unknown'])
+            ->addColumn('latitude', 'string', ['limit' => 30, 'default' => 0])
+            ->addColumn('longitude', 'string', ['limit' => 30, 'default' => 0])
+            ->addColumn('isp', 'string', ['limit' => MysqlAdapter::TEXT_SMALL, 'default' => 'Unknown'])
+            ->addColumn('useragent', 'string', ['limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('referer_url', 'string', ['limit' => MysqlAdapter::TEXT_SMALL])
+            ->create();
 
     }
 }
