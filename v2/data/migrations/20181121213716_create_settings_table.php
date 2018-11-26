b975e18a2fdd8f91bda530d68cab111614ecad0d
bernard-ng <ngandubernard@gmail.com>
2018-11-26 23:03:05 +0200

    [Update] add migration logic to settings

--- v2/data/migrations/20181121213716_create_settings_table.php
+++ v2/data/migrations/20181121213716_create_settings_table.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use Phinx\Db\Adapter\MysqlAdapter;
 use Phinx\Migration\AbstractMigration;
 
 class CreateSettingsTable extends AbstractMigration
@@ -32,6 +33,19 @@ class CreateSettingsTable extends AbstractMigration
      */
     public function change()
     {
+        $this->table('settings')
+            ->addColumn('email', 'string', ['limit' => MysqlAdapter::TEXT_SMALL])
+            ->addColumn('realtime_protection', 'integer', ['limit' => 1, 'default' => 1])
+            ->addColumn('mail_notifications', 'integer', ['limit' => 1, 'default' => 1])
+            ->addColumn('ip_detection', 'integer', ['limit' => 1, 'default' => 0])
+            ->addColumn('countryban_blacklist', 'integer', ['limit' => 1, 'default' => 1])
+            ->addColumn('live_traffic', 'integer', ['limit' => 1, 'default' => 0])
+            ->addColumn('jquery_include', 'integer', ['limit' => 1, 'default' => 0])
+            ->addColumn('error_reporting', 'integer', ['limit' => MysqlAdapter::INT_MEDIUM, 'default' => 5])
+            ->addColumn('display_errors', 'integer', ['limit' => 1, 'default' => 0])
+            ->addColumn('fixed_layout', 'integer', ['limit' => 1, 'default' => 0])
+            ->addColumn('boxed_layout', 'integer', ['limit' => 1, 'default' => 0])
+            ->create();
 
     }
 }
