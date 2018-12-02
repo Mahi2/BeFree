17911f868426919fda5f8b958761afac75715f3b
bernard-ng <ngandubernard@gmail.com>
2018-12-02 22:58:03 +0200

    [Update] add the logs directory to gitignore

---
+++ v2/src/Services/ErrorHandlerService.php
@@ -0,0 +1,11 @@
+<?php
+namespace Befree\Services;
+
+/**
+ * Class ErrorResgisterService
+ * @package Befree\Services
+ */
+class ErrorRegisterService
+{
+    
+}
--- v2/src/Services/LogWriterService.php
+++ v2/src/Services/LogWriterService.php
@@ -3,13 +3,13 @@
  * Created by PhpStorm.
  * User: Bernard-ng
  * Date: 12/2/2018
- * Time: 10:17 PM
+ * Time: 10:22 PM
  */
 
 namespace Befree\Services;
 
 
-class ErrorResgisterService
+class LogWriterService
 {
 
 }
