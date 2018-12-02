21b5a110dbdf4ab2e3bfc832c1ecc1357c642f9f
bernard-ng <ngandubernard@gmail.com>
2018-12-03 00:55:39 +0200

    [Add] render a nice page design

--- v2/src/Services/ErrorHandlerService.php
+++ v2/src/Services/ErrorHandlerService.php
@@ -33,8 +33,7 @@ class ErrorHandlerService
                 $this->log->write($e);
             } else {
                 echo $this->header();
-                print_r($e, true);
-                echo "</pre>";
+                echo $this->body(print_r($e, true), $e);
             }
         });
 
@@ -43,8 +42,7 @@ class ErrorHandlerService
                 $this->log->write($args);
             } else {
                 echo $this->header();
-                print_r($args, true);
-                echo "</pre>";
+                echo $this->body(print_r($args, true));
             }
         });
     }
@@ -56,19 +54,41 @@ class ErrorHandlerService
      */
     private function header(): string
     {
-        $author = "bernard ng <nganduberanrd@gmail.com>";
-        $generateAt = date("Y-m-d", time());
+        $generateAt = date("D, d M Y H:i", time());
         $header = <<< EOF
-<div>
-    <h3 style='color: red'>!! BUG !!</h3>
-    <span>GENERATED AT : <strong>{$generateAt}</strong></span>
-    <span>DEVELOPED BY : <strong>{$author}</strong></span>
-    <strong>SEE : <a href="https://github.com/bernard-ng">bernard-ng (github)</a></strong>
+<style>
+*{ box-sizing: border-box;}
+body,html{margin:0 0;background:#f8f9fa;font-family:sans-serif;}
+a{font-color:#007bff}
+a{color:#007bff;text-decoration:none;background-color:transparent;}
+a:hover{color:#0056b3;text-decoration:none}
+</style>
+<div style="background: #343a40; color: #ccc; padding: 25px; border-bottom: 5px solid rgba(255,255,255,0.4);">
+    <span style='color: #007bff; font-size: 2.5em; font-weight: bold;'>Befree Oups !</span><br>
+    <span>Generated at : <strong>{$generateAt}</strong></span><br>
+    <span>Developed  by : <a href="https://github.com/bernard-ng">bernard-ng</a></span><br>
 </div>
-<br><hr>
-<pre>
 EOF;
         return $header;
+    }
+
+
+    private function body(string $content, $e = null): string
+    {
+        $title = $e->getMessage() ?? '';
+        $class = get_class($e) ?? '';
+        return <<< EOF
+<style>
+pre{display: block;color: #212529}
+pre code{font-size: inherit;color: inherit;word-break: normal}
+hr{margin-top: 1rem;margin-bottom: 1rem;border: 0;border-top: 1px solid rgba(0,0,0,.1)}
+.jumbotron {padding: 2rem 1rem;margin-bottom: 2rem;background-color: #e9ecef;border-radius: .3rem}
+</style>
+<div style="padding: 25px;">
+<h2><strong style="color: red">$class</strong> : $title</h2><hr>
+<div class="jumbotron"><pre>$content</pre></div>
+</div>
+EOF;
 
     }
 }
