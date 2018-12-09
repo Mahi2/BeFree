ea085e1346a3a373fc02bb3b06c9a233476d2fc4
bernard-ng <ngandubernard@gmail.com>
2018-12-09 22:16:20 +0200

    [Fix] renamed the namespace for SpamSettingsRepository

--- v2/src/Application/Repositories/SpamSettingsRepository.php
+++ v2/src/Application/Repositories/SpamSettingsRepository.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Befree\Repositories;
+namespace Befree\Applications\Repositories;
 
 use Befree\Repository;
 
 /**
  * Class SpamSettingsRepository
- * @package Befree\Repositories
+ * @package Befree\Applications\Repositories
  */
 class SpamSettingsRepository extends Repository
 {
