1e5c49253ad7878dd2b1ec6171ef1a58591ef0a1
bernard-ng <ngandubernard@gmail.com>
2018-12-09 23:50:54 +0200

    [Update] print the befree dashboard view

---
+++ v2/src/Application/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bernard-ng
+ * Date: 12/9/2018
+ * Time: 11:40 PM
+ */
+
+namespace Befree\Applications\Controllers;
+
+
+class Controller
+{
+
+}
--- v2/src/Application/Controllers/DashboardController.php
+++ v2/src/Application/Controllers/DashboardController.php
@@ -1,8 +1,28 @@
 <?php
 namespace Befree\Applications\Controllers;
 
+use Psr\Container\ContainerInterface;
 
+
+/**
+ * Class DashboardController
+ * @package Befree\Applications\Controllers
+ */
 class DashboardController
 {
 
+    /**
+     * DashboardController constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        //parent::__construct($container);
+    }
+
+
+    public function index()
+    {
+        echo "hello world";
+    }
 }
