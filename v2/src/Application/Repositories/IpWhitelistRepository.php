ebaa12acd2de663f47807ac12e0d8144a27ed695
bernard-ng <ngandubernard@gmail.com>
2018-12-09 22:10:51 +0200

    [Fix] renamed the namespace for IpWhitelistRepository

--- v2/src/Application/Repositories/IpWhitelistRepository.php
+++ v2/src/Application/Repositories/IpWhitelistRepository.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Befree\Repositories;
+namespace Befree\Applications\Repositories;
 
 use Befree\Repository;
 
 /**
  * Class IpWhitelistRepository
- * @package Befree\Repositories
+ * @package Befree\Applications\Repositories
  */
 class IpWhitelistRepository extends Repository
 {
