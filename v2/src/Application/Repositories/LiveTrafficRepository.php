0a4abfd8bf884d84c5020d7e4515d1841e6a953a
bernard-ng <ngandubernard@gmail.com>
2018-12-09 22:11:16 +0200

    [Fix] renamed the namespace for LiveTrafficRepository

--- v2/src/Application/Repositories/LiveTrafficRepository.php
+++ v2/src/Application/Repositories/LiveTrafficRepository.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Befree\Repositories;
+namespace Befree\Applications\Repositories;
 
 use Befree\Repository;
 
 /**
  * Class LiveTrafficRepository
- * @package Befree\Repositories
+ * @package Befree\Applications\Repositories
  */
 class LiveTrafficRepository extends Repository
 {
