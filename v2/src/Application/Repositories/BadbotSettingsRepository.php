5f91a5b3f4fee82ae88233f9548630ba5ab74297
bernard-ng <ngandubernard@gmail.com>
2018-12-09 22:08:31 +0200

    [Fix] renamed the namespace for BadbotSettingsRepository

--- v2/src/Application/Repositories/BadbotSettingsRepository.php
+++ v2/src/Application/Repositories/BadbotSettingsRepository.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Befree\Repositories;
+namespace Befree\Applications\Repositories;
 
 use Befree\Repository;
 
 /**
  * Class BadbotSettingsRepository
- * @package Befree\Repositories
+ * @package Befree\Applications\Repositories
  */
 class BadbotSettingsRepository extends Repository
 {
