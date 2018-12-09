915687bbd8e9919a836ac2a4f4579067c93353f2
bernard-ng <ngandubernard@gmail.com>
2018-12-09 22:10:33 +0200

    [Fix] renamed the namespace for DnsblDatabaseRepository

--- v2/src/Application/Repositories/DnsblDatabaseRepository.php
+++ v2/src/Application/Repositories/DnsblDatabaseRepository.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Befree\Repositories;
+namespace Befree\Applications\Repositories;
 
 use Befree\Repository;
 
 /**
  * Class DnsblDatabaseRepository
- * @package Befree\Repositories
+ * @package Befree\Applications\Repositories
  */
 class DnsblDatabaseRepository extends Repository
 {
