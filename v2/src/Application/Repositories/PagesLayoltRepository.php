b469a5745f809294ae30330f906a775631431405
bernard-ng <ngandubernard@gmail.com>
2018-12-09 22:15:14 +0200

    [Fix] renamed the namespace for PagesLayltRepository

--- v2/src/Application/Repositories/PagesLayoltRepository.php
+++ v2/src/Application/Repositories/PagesLayoltRepository.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Befree\Repositories;
+namespace Befree\Applications\Repositories;
 
 use Befree\Repository;
 
 /**
  * Class PagesLayoltRepository
- * @package Befree\Repositories
+ * @package Befree\Applications\Repositories
  */
 class PagesLayoltRepository extends Repository
 {
