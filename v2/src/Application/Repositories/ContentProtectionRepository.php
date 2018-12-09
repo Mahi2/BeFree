baea48ba71c0391422a5ae0259c44b3b97feae8a
bernard-ng <ngandubernard@gmail.com>
2018-12-09 22:10:06 +0200

    [Fix] renamed the namespace for ContentProtectionRepository

--- v2/src/Application/Repositories/ContentProtectionRepository.php
+++ v2/src/Application/Repositories/ContentProtectionRepository.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Befree\Repositories;
+namespace Befree\Applications\Repositories;
 
 use Befree\Repository;
 
 /**
  * Class ContentProtectionRepository
- * @package Befree\Repositories
+ * @package Befree\Applications\Repositories
  */
 class ContentProtectionRepository extends Repository
 {
