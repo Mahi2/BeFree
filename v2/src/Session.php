df485ace316221f266b92a51c76c8b33a0274f1e
bernard-ng <ngandubernard@gmail.com>
2018-12-15 18:14:47 +0200

    [Update] add the copyright header for source files

--- v2/src/Session.php
+++ v2/src/Session.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Bernard-ng
- * Date: 11/18/2018
- * Time: 4:41 PM
+ *   This file is part of the Befree.
+ *
+ *   @copyright   Henrique Mukanda <mahi2hm@outlook.fr>
+ *   @copyright   Bernard ngandu <ngandubernard@gmail.com>
+ *   @link    https://github.com/Mahi2/BeFree
+ *   @link    https://github.com/bernard-ng/Befree
+ *   @license   http://framework.zend.com/license/new-bsd New BSD License
+ *
+ *   For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
  */
 
 namespace Befree;
