62395df9e3d63d5b10b60c5747ac1dc3dcd3deb3
bernard-ng <ngandubernard@gmail.com>
2019-01-12 21:29:25 +0200

    [New] create twig extension for router

---
+++ v2/config/extensions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bernard-ng
+ * Date: 1/12/2019
+ * Time: 9:22 PM
+ */
---
+++ v2/src/Router/RouterTwigExtension.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bernard-ng
+ * Date: 1/12/2019
+ * Time: 9:22 PM
+ */
+
+namespace Befree\Router;
+
+
+class RouterTwigExtension
+{
+
+}
