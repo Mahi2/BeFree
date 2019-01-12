4363a5602d4ae878ad51d4cf86a589e2f74d62c1
bernard-ng <ngandubernard@gmail.com>
2019-01-12 21:30:09 +0200

    [Update] register router twig extension

--- v2/config/extensions.php
+++ v2/config/extensions.php
@@ -1,7 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Bernard-ng
- * Date: 1/12/2019
- * Time: 9:22 PM
+ *   This file is part of the Befree.
+ *
+ *   @copyright   Henrique Mukanda <mahi2hm@outlook.fr>
+ *   @copyright   Bernard ngandu <ngandubernard@gmail.com>
+ *   @link    https://github.com/Mahi2/BeFree
+ *   @link    https://github.com/bernard-ng/Befree
+ *   @license   http://framework.zend.com/license/new-bsd New BSD License
+ *
+ *   For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
  */
+
+return [
+    \Befree\Router\RouterTwigExtension::class
+];
