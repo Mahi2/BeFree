74fa7c9963270f57689f7171d549c9f14ac9a80a
bernard-ng <ngandubernard@gmail.com>
2019-01-13 02:08:51 +0200

    [Update] add new routes and change the names of controllers

--- v2/config/routes.php
+++ v2/config/routes.php
@@ -3,4 +3,22 @@
  * befree application's routes
  */
 
-$router->get('/', "Dashboard@index", "dashboard.index");
+use Befree\Application\Controllers\{
+    DashboardController,
+    Tools\PortScannerController
+};
+
+
+$router->get('/', [DashboardController::class, 'index'], "dashboard.index");
+
+
+//ANALYTICS
+
+
+// SECURITY
+
+
+
+// TOOLS
+$router->get('/port-scanner', [PortScannerController::class, 'index'], 'portScanner');
+$router->post('/port-scanner', [PortScannerController::class, 'index'], 'portScanner');
