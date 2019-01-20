c06ea3654bc148c6e6fc482e2a66aa66071f5b47
bernard-ng <ngandubernard@gmail.com>
2019-01-20 09:27:05 +0200

    [New] create befree dashboard controllers

--- v2/config/routes.php
+++ v2/config/routes.php
@@ -4,8 +4,7 @@
  */
 
 use Befree\Application\Controllers\{
-    DashboardController,
-    Tools\PortScannerController
+    DashboardController, Tools\ErrorMonitoringController, Tools\HtaccessEditorController, Tools\IpBlackListCheckerController, Tools\PortScannerController
 };
 
 
@@ -20,5 +19,11 @@ $router->get('/', [DashboardController::class, 'index'], "dashboard.index");
 
 
 // TOOLS
-$router->get('/port-scanner', [PortScannerController::class, 'index'], 'portScanner');
-$router->post('/port-scanner', [PortScannerController::class, 'index'], 'portScanner');
+$router->get('tools/port-scanner', [PortScannerController::class], 'tools.portScanner');
+$router->post('tools/port-scanner', [PortScannerController::class], 'tools.portScanner');
+$router->get('tools/htaccess-editor', [HtaccessEditorController::class], 'tools.htaccessEditor');
+$router->post('tools/htaccess-editor', [HtaccessEditorController::class], 'tools.htaccessEditor');
+$router->get('tools/error-monitoring', [ErrorMonitoringController::class], 'tools.errorMonitoring');
+$router->post('tools/error-monitoring', [ErrorMonitoringController::class], 'tools.errorMonitoring');
+$router->get('tools/ip-blacklist-checker', [IpBlackListCheckerController::class], 'tools.ipBlackListChecker');
+$router->post('tools/ip-blacklist-checker', [IpBlackListCheckerController::class], 'tools.ipBlackListChecker');
