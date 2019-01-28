de56e372c1f95440b986462c7ad7942a7c9ed471
bernard-ng <ngandubernard@gmail.com>
2019-01-28 20:41:16 +0200

    [Update] refactor routes and add tools routes

--- v2/config/routes.php
+++ v2/config/routes.php
@@ -16,18 +16,29 @@ $router->get('/', [DashboardController::class, 'index'], "dashboard.index");
 
 
 //ANALYTICS
+analytics_routes : {
 
+}
 
 // SECURITY
+security_routes : {
 
+}
 
 
 // TOOLS
-$router->get('tools/port-scanner', [PortScannerController::class], 'tools.portScanner');
-$router->post('tools/port-scanner', [PortScannerController::class], 'tools.portScanner');
-$router->get('tools/htaccess-editor', [HtaccessEditorController::class], 'tools.htaccessEditor');
-$router->post('tools/htaccess-editor', [HtaccessEditorController::class], 'tools.htaccessEditor');
-$router->get('tools/error-monitoring', [ErrorMonitoringController::class], 'tools.errorMonitoring');
-$router->post('tools/error-monitoring', [ErrorMonitoringController::class], 'tools.errorMonitoring');
-$router->get('tools/ip-blacklist-checker', [IpBlackListCheckerController::class], 'tools.ipBlackListChecker');
-$router->post('tools/ip-blacklist-checker', [IpBlackListCheckerController::class], 'tools.ipBlackListChecker');
+tools_routes : {
+    $router->get('tools/port-scanner', [PortScannerController::class], 'tools.portScanner');
+    $router->post('tools/port-scanner', [PortScannerController::class], 'tools.portScanner');
+    $router->get('tools/htaccess-editor', [HtaccessEditorController::class], 'tools.htaccessEditor');
+    $router->post('tools/htaccess-editor', [HtaccessEditorController::class], 'tools.htaccessEditor');
+    $router->get('tools/error-monitoring', [ErrorMonitoringController::class], 'tools.errorMonitoring');
+    $router->post('tools/error-monitoring', [ErrorMonitoringController::class], 'tools.errorMonitoring');
+    $router->get('tools/ip-blacklist-checker', [IpBlackListCheckerController::class], 'tools.ipBlackListChecker');
+    $router->post('tools/ip-blacklist-checker', [IpBlackListCheckerController::class], 'tools.ipBlackListChecker');
+    $router->get('tools/html-encrypter', [], 'tools.htmlEncrypter');
+    $router->post('tools/html-encrypter', [], 'tools.htmlEncrypter');
+    $router->get('tools/password-generator', [], 'tools.passwordGenerator');
+    $router->get('tools/hashing', [], 'tools.hashing');
+    $router->post('tools/hashing', [], 'tools.hashing');
+}
