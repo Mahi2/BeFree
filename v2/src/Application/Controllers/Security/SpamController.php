8e402b0d8c0098b62f0dcfe8721490848245ac4f
bernard-ng <ngandubernard@gmail.com>
2019-01-28 20:54:49 +0200

    [New] create SpamController

---
+++ v2/src/Application/Controllers/Security/SpamController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *   This file is part of the Befree.
+ *
+ *   @copyright   Henrique Mukanda <mahi2hm@outlook.fr>
+ *   @copyright   Bernard ngandu <ngandubernard@gmail.com>
+ *   @link    https://github.com/Mahi2/BeFree
+ *   @link    https://github.com/bernard-ng/Befree
+ *   @license   http://framework.zend.com/license/new-bsd New BSD License
+ *
+ *   For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ */
+
+
+
+namespace Befree\Application\Controllers\Security;
+
+
+use Befree\Application\Controllers\Controller;
+
+/**
+ * Class SpamController
+ * @package Befree\Application\Controllers\Security
+ */
+class SpamController extends Controller
+{
+
+}
