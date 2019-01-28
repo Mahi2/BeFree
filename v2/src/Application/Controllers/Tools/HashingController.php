707d264d18ce11bd242d8312e6dc9e46423365a1
bernard-ng <ngandubernard@gmail.com>
2019-01-28 20:50:40 +0200

    [New] create missing tools controllers

--- v2/src/Application/Controllers/Tools/HashingController.php
+++ v2/src/Application/Controllers/Tools/HashingController.php
@@ -1,15 +1,28 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Bernard-ng
- * Date: 1/28/2019
- * Time: 8:42 PM
+ *   This file is part of the Befree.
+ *
+ *   @copyright   Henrique Mukanda <mahi2hm@outlook.fr>
+ *   @copyright   Bernard ngandu <ngandubernard@gmail.com>
+ *   @link    https://github.com/Mahi2/BeFree
+ *   @link    https://github.com/bernard-ng/Befree
+ *   @license   http://framework.zend.com/license/new-bsd New BSD License
+ *
+ *   For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
  */
+
 
 namespace Befree\Application\Controllers\Tools;
 
 
-class HashingController
+use Befree\Application\Controllers\Controller;
+
+/**
+ * Class HashingController
+ * @package Befree\Application\Controllers\Tools
+ */
+class HashingController extends Controller
 {
 
 }
